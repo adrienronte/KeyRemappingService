3cac4cf41e88cd894c3033f321cf2e411f9d34f2
BadUsername <adrien.ronte@gmail.com>
2026-09-14 18:28:24 +0200

    Add project files.

---
+++ Controllers/KeyRemappingController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace KeyRemappingService.Controllers
+{
+    [ApiController]
+    [Route("api/controller")]
+    public class KeyRemappingController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Le web service fonctionne !");
+        }
+    }
+}
